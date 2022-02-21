316f603a5b9428f799b1c642901207e55de2365b
NickItweb <98798052+NickItweb@users.noreply.github.com>
2022-02-21 21:12:32 +0530

    demo

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const name = "Angela";
+const num = 7;
+
 ReactDOM.render(
   <div>
-    <h1>What to SharedWorker</h1>
+    <h1>My Favourite {name}!</h1>
+    <p>Your luck Number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
